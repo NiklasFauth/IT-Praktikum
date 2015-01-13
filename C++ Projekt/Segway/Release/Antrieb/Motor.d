7d76807cedb2360e80af1282256c80e047370dfb
Aniles <selina.eckel@gmx.de>
2015-01-13 10:09:10 +0100

    Vorlaufige Dokumentation, es fehlen die Punkte Integration, Schnittstellen und eure Beschreibung der Methoden

---
+++ C++ Projekt/Segway/Release/Antrieb/Motor.d
@@ -0,0 +1,9 @@
+Antrieb/Motor.d Antrieb/Motor.o Antrieb/Motor.o: ../Antrieb/Motor.cpp \
+ ../Antrieb/Motor.h ../Antrieb/../Configuration/Configuration.h \
+ ../Antrieb/PWM.h
+
+../Antrieb/Motor.h:
+
+../Antrieb/../Configuration/Configuration.h:
+
+../Antrieb/PWM.h:
